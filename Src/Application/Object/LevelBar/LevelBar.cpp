2c56947ad5403c96a23d0d2d34da27b53203480b
PERDERE4412 <kd1300912@st.kobedenshi.ac.jp>
2024-05-27 23:17:49 +0900

    バックアップ
    バックアップ

---
+++ Src/Application/Object/LevelBar/LevelBar.cpp
@@ -0,0 +1,39 @@
+﻿#include "LevelBar.h"
+
+#include "../../Lib/AssetManager/AssetManager.h"
+
+void LevelBar::Update()
+{
+}
+
+void LevelBar::DrawSprite()
+{
+	// フレーム
+	KdShaderManager::Instance().m_spriteShader.SetMatrix(m_frame.mat);
+	KdShaderManager::Instance().m_spriteShader.DrawTex(m_frame.pTex, 0, 0, m_frame.width, m_frame.height, &m_frame.rect);
+
+	// バー
+	KdShaderManager::Instance().m_spriteShader.SetMatrix(m_bar.mat);
+	KdShaderManager::Instance().m_spriteShader.DrawTex(m_bar.pTex, 0, 0, m_bar.width, m_bar.height, &m_bar.rect);
+}
+
+void LevelBar::Init()
+{
+	// フレーム
+	m_frame.pTex = AssetManager::Instance().GetTex("levelFrame");
+	m_frame.pos = { 0,0,0 };
+	m_frame.Set();
+
+	// バー
+	m_bar.pTex = AssetManager::Instance().GetTex("levelBar");
+	m_bar.pos = { 0,0,0 };
+	m_bar.Set();
+}
+
+void LevelBar::TexData::Set()
+{
+	mat = Math::Matrix::CreateTranslation(pos);
+	width = pTex->GetWidth();
+	height = pTex->GetHeight();
+	rect = { 0,0,width,height };
+}
---
+++ Src/Application/Object/LevelBar/LevelBar.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+class LevelBar :public KdGameObject
+{
+public:
+
+	LevelBar() { Init(); }
+	~LevelBar()override {}
+
+	void Update()override;
+	void DrawSprite()override;
+
+private:
+
+	void Init()override;
+
+	struct  TexData
+	{
+		KdTexture* pTex;
+		Math::Vector3 pos;
+		Math::Vector3 size;
+		Math::Matrix mat;
+		Math::Rectangle rect;
+		int width;
+		int height;
+		void Set();
+	};
+
+	TexData m_frame;
+	TexData m_bar;
+};
